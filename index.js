6fde3589dda47dbbd6fbd3b7bc4da7bf3d22b464
hsngirlswhocode <hsngirlswhocode@gmail.com>
2026-01-09 23:17:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const guess = 6;
+const wLen = 5;
+let numGuess = 0;
+let secret = [TULIP, SEVEN, ELITE, LAMBS];
+let answer = "";
+
+let currentW = [];
+let currentG = 0;
+
+function game () {
+  answer = secret[(int) (Math.random()*secret.length)];
+  for (int i = 0; i < guess; i++) {
+    
+  }
+}
